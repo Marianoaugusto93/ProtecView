6be2cc937f493385ac0ca6fbb4d946db678e2a52
Augusto <augustocesar.mariano@gmail.com>
2025-10-30 15:11:57 -0300

    V1 - correção

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+# Ficheiro: app.py
+
 import dash
 
-# --- 1. Inicializar a Aplicação Dash ---
-# Esta é a única coisa que este ficheiro fará.
-# Os outros ficheiros irão importar esta variável 'app'.
-# O __name__ e a pasta 'assets' continuam a funcionar da mesma forma.
+# 1. Inicializa a aplicação
 app = dash.Dash(__name__)
+
+# 2. CONFIGURAÇÃO CRÍTICA
+# Esta linha diz ao Dash para não verificar os IDs dos callbacks
+# no momento em que os ficheiros são importados.
+# Isto é essencial para aplicações com múltiplos ficheiros (como a nossa),
+# onde o layout é definido num ficheiro e os callbacks noutro.
+app.config.suppress_callback_exceptions = True
